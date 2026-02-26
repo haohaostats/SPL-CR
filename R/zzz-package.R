27f86d22fad42cd81470a8e437f2703ff1e2e28a
Hao Chen <haocstats@gmail.com>
2026-02-27 10:31:56 +1100

    Register native routines via NAMESPACE useDynLib and fix .Call wrappers

---
+++ R/zzz-package.R
@@ -0,0 +1,7 @@
+#' splcr: Smoothed Predictive Likelihood for Competing Risks
+#'
+#' Internal package metadata and native routine registration.
+#'
+#' @keywords internal
+#' @useDynLib splcr, .registration = TRUE
+"_PACKAGE"
